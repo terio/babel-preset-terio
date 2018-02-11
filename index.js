ecb5523cd01ae0f849cc713de8b4a2cd5b9a814e
aditya subramanyam <subramanyam963@gmail.com>
2018-02-11 21:19:19 +0530

    removed unneeded dsiplay name transform

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = function(api, opts = {}) {
     return {
         plugins: [
             require('@babel/plugin-syntax-jsx'),
-            require('@babel/plugin-transform-react-display-name'),
             [
                 require('@babel/plugin-transform-react-jsx'),
                 {
