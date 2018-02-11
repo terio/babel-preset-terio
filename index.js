291aa163030db59cd2f19dd56a694a264860bfdd
aditya subramanyam <subramanyam963@gmail.com>
2018-02-11 17:13:55 +0530

    removing @babel/preset-env

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 module.exports = function(api, opts = {}) {
     return {
-        presets: [
-            require('@babel/preset-env')
-        ],
         plugins: [
             require('@babel/plugin-syntax-jsx'),
             require('@babel/plugin-transform-react-display-name'),
             [
                 require('@babel/plugin-transform-react-jsx'),
                 {
-                    pragma: opts.pragma || 'loki.createElement'
+                    pragma: opts.pragma || 'loki.createVirtualNode'
                 }
             ]
         ]
